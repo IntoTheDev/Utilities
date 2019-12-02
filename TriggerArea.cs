b00aa95aab48b2933454e14e388d62b2f00b1d31
Vladislav Kinyashov <53948684+IntoTheDev@users.noreply.github.com>
2019-12-02 22:04:48 +0500

    Update

---
+++ TriggerArea.cs
@@ -0,0 +1,36 @@
+﻿using ToolBox.Attributes;
+using ToolBox.Groups;
+using UnityEngine;
+using UnityEngine.Events;
+
+namespace ToolBox.Runtime
+{
+	[DisallowMultipleComponent, RequireComponent(typeof(BoxCollider2D))]
+	public class TriggerArea : MonoBehaviour
+	{
+		[SerializeField, ReorderableList] private EventsData[] eventsData = null;
+
+		private int index = 0;
+
+		private void OnTriggerEnter2D(Collider2D collision) => eventsData[index].Call(collision.gameObject);
+
+		public void SetIndex(int index) => this.index = index;
+
+		[System.Serializable]
+		private struct EventsData
+		{
+#if UNITY_EDITOR
+			[SerializeField] private string editorName;
+#endif
+
+			[SerializeField] private Group[] groups;
+			[SerializeField] private UnityEvent events;
+
+			public void Call(GameObject entity)
+			{
+				if (Group.IsEntityInGroups(entity, groups, CheckType.AllGroups))
+					events?.Invoke();
+			}
+		}
+	}
+}
