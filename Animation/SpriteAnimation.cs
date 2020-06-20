eab1df079550b18d3f288502d4cceef8466a2158
Vladislav Kinyashov <53948684+IntoTheDev@users.noreply.github.com>
2020-06-20 16:45:18 +0500

    Update

---
+++ Animation/SpriteAnimation.cs
@@ -0,0 +1,14 @@
+﻿using Sirenix.OdinInspector;
+using UnityEngine;
+
+[CreateAssetMenu(menuName = "ToolBox/Sprite Animation"), AssetSelector]
+public class SpriteAnimation : ScriptableObject
+{
+	[SerializeField, AssetSelector, PreviewField] private Sprite[] _sprites = null;
+
+	public void LoadSprites(ref Sprite[] sprites)
+	{
+		sprites = new Sprite[_sprites.Length];
+		_sprites.CopyTo(sprites, 0);
+	}
+}
---
+++ Switcher.cs
@@ -0,0 +1,25 @@
+﻿using Sirenix.OdinInspector;
+using ToolBox.Reactors;
+using UnityEngine;
+
+namespace ToolBox.Utilities
+{
+    public class Switcher : MonoBehaviour, IReactor
+    {
+        [ShowInInspector, ReadOnly] private int _reactorIndex = 0;
+        [SerializeField, PageList] private Reactor[] _reactors = null;
+
+        private void Awake()
+        {
+            for (int i = 0; i < _reactors.Length; i++)
+                _reactors[i].Setup();
+        }
+
+        [Button]
+        public void HandleReaction()
+        {
+            _reactors[_reactorIndex].SendReaction();
+            _reactorIndex = Extensions.Extensions.RoundRobin(_reactorIndex, _reactors.Length);
+        }
+    }
+}
