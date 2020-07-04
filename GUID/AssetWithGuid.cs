b4b3347bb7095b12f2264417beabc9086c9fbb15
Vladislav Kinyashov <53948684+IntoTheDev@users.noreply.github.com>
2020-07-04 19:08:37 +0500

    GUID

---
+++ GUID/AssetWithGuid.cs
@@ -0,0 +1,22 @@
+﻿using Sirenix.OdinInspector;
+using System;
+using UnityEngine;
+
+namespace ToolBox.Utilities
+{
+	[CreateAssetMenu(menuName = "ToolBox/Guid/Asset")]
+	public class AssetWithGuid : ScriptableObject
+	{
+		[SerializeField, ReadOnly] private string _value = "";
+
+		public string Value => _value;
+
+#if UNITY_EDITOR
+		private void OnEnable()
+		{
+			if (string.IsNullOrEmpty(_value))
+				_value = Guid.NewGuid().ToString();
+		}
+#endif
+	}
+}
---
+++ GUID/GuidGenerator.cs
@@ -0,0 +1,33 @@
+﻿using Sirenix.OdinInspector;
+using System;
+using TMPro;
+using ToolBox.Reactors;
+using UnityEngine;
+
+namespace ToolBox.Utilities
+{
+	[DisallowMultipleComponent]
+	public class GuidGenerator : MonoBehaviour
+	{
+		[SerializeField, ReadOnly] private string _value = "";
+
+		public string Value => _value;
+
+#if UNITY_EDITOR
+		private void OnEnable()
+		{
+			if (string.IsNullOrEmpty(_value))
+				_value = Guid.NewGuid().ToString();
+		}
+#endif
+	}
+
+	public class GuidDisplay : IReactor
+	{
+		[SerializeField, Required, AssetSelector] private AssetWithGuid _asset = null;
+		[SerializeField, Required, SceneObjectsOnly] private TMP_Text _view = null;
+
+		public void HandleReaction() =>
+			_view.text = _asset.Value;
+	}
+}
